a0e332f30255faf8b482e66c3f8c8de5349dc2c1
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-05-31 00:27:17 -0300

    adicionando tema da postagem

--- BlogPessoal/src/main/java/org/generation/BlogPessoal/repository/PostagemRepository.java
+++ BlogPessoal/src/main/java/org/generation/BlogPessoal/repository/PostagemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+public interface PostagemRepository extends JpaRepository<Postagem, Long> { // Herança da JPA, usando a classe e o tipo do ID
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
 	
 }
