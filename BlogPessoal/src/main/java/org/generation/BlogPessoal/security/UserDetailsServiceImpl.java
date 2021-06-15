2cca42d0170ca00898eb680d297b6837b9651ccb
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-06-15 09:37:13 -0300

    Adicionando UserDetailsImpl e UserDetailsServiceImpl no security

---
+++ BlogPessoal/src/main/java/org/generation/BlogPessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package org.generation.BlogPessoal.security;
+
+import java.util.Optional;
+
+import org.generation.BlogPessoal.model.Usuario;
+import org.generation.BlogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(username);
+		usuario.orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+
+		return usuario.map(UserDetailsImpl::new).get();
+
+	}
+	
+}
